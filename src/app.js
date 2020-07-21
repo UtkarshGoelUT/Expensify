d722e6881ea2b25aa103d12ee1610cdb9dbfb099
Utkarsh Goel <ugoel911@gmail.com>
2020-07-21 23:25:30 +0530

    Server ready for deployment

--- src/app.js
+++ src/app.js
@@ -5,22 +5,10 @@ import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 200 }));
-store.dispatch(addExpense({ description: "Rent", amount: 1000, createdAt: 400 }));
-store.dispatch(addExpense({ description: "Gas bill" }));
-store.dispatch(setTextFilter('water'));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-setTimeout(() => { store.dispatch(setTextFilter('bill')) }, 3000);
-console.log(visibleExpenses);
-console.log(store.getState());
 
 const jsx = (
     <Provider store={store}><AppRouter /></Provider>
